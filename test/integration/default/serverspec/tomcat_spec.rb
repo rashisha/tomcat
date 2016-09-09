c8b28dc57d5b0c9917d9c7bde6f5142c3f4ed29f
Rashi Sharma <rashi_s@hcl.com>
2016-09-09 11:38:54 +0530

    serverspec tests

--- test/integration/default/serverspec/tomcat_spec.rb
+++ test/integration/default/serverspec/tomcat_spec.rb
@@ -7,3 +7,9 @@ end
 describe file('/var/lib//tomcat/conf/server.xml') do
   it { should exist }
 end
+
+# Tomcat service
+describe service('tomcat7') do
+    it { should be_enabled }
+    it { should be_running }
+end
